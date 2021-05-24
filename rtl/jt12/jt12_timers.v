529945070b6146b76631de273282e97ef7318137
birdybro <aberusugi@gmail.com>
2021-05-23 21:31:47 -0600

    update jt12 more
    Still getting PCM at 2x speed for some reason... going to have to look at this later, maybe next weekend...

--- rtl/jt12/jt12_timers.v
+++ rtl/jt12/jt12_timers.v
@@ -1,46 +1,46 @@
 /*  This file is part of JT12.
 
-	JT12 is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
+  JT12 is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-	JT12 is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+  JT12 is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with JT12.  If not, see <http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU General Public License
+  along with JT12.  If not, see <http://www.gnu.org/licenses/>.
 
-	Author: Jose Tejada Gomez. Twitter: @topapate
-	Version: 1.0
-	Date: 14-2-2017
+  Author: Jose Tejada Gomez. Twitter: @topapate
+  Version: 1.0
+  Date: 14-2-2017
 
-	YM3438_APL.pdf
-	Timer A = 144*(1024-NA)/Phi M
-	Timer B = 2304*(256-NB)/Phi M
-	*/
+  YM3438_APL.pdf
+  Timer A = 144*(1024-NA)/Phi M
+  Timer B = 2304*(256-NB)/Phi M
+  */
 
 `timescale 1ns / 1ps
 
 module jt12_timers(
-  input			clk,
-  input			rst,
-  input			clk_en /* synthesis direct_enable */,
-  input			zero,
-  input [9:0]	value_A,
-  input [7:0]	value_B,
-  input 		load_A,
-  input 		load_B,
-  input 		clr_flag_A,
-  input 		clr_flag_B,
-  input 		enable_irq_A,
-  input 		enable_irq_B,
-  output 	 	flag_A,
-  output 	 	flag_B,
-  output		overflow_A,
-  output 	 	irq_n
+  input       clk,
+  input       rst,
+  input       clk_en /* synthesis direct_enable */,
+  input       zero,
+  input [9:0] value_A,
+  input [7:0] value_B,
+  input       load_A,
+  input       load_B,
+  input       clr_flag_A,
+  input       clr_flag_B,
+  input       enable_irq_A,
+  input       enable_irq_B,
+  output      flag_A,
+  output      flag_B,
+  output      overflow_A,
+  output      irq_n
 );
 
 parameter num_ch = 6;
